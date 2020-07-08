f63ef0f4f6e128e08d739ec4c0e29edc60592b9a
Eric Mader <emader@gmx.us>
2020-07-08 11:45:34 -1000

    More PathUtilities, tests including midpoint of line and intersection of lines and rectangles

--- PathUtilities.py
+++ PathUtilities.py
@@ -8,11 +8,47 @@ Created on July 7, 2020
 
 import math
 
-def getDeltas(segment):
+def minMax(a, b):
+    return (a, b) if a <= b else (b, a)
+
+def endPoints(segment):
     p0x, p0y = segment[0]
-    p1x, p1y = segment[1]
+    p1x, p1y = segment[-1]
+
+    return (p0x, p0y, p1x, p1y)
+
+def getDeltas(segment):
+    p0x, p0y, p1x, p1y = endPoints(segment)
 
     return (p1x - p0x, p1y - p0y)
+
+# Fix this to work for curves too...
+def bounds(segment):
+    p0x, p0y, p1x, p1y = endPoints(segment)
+    left, right = minMax(p0x, p1x)
+    bottom, top = minMax(p0y, p1y)
+
+    return [(left, top), (right, bottom)]
+
+def bounds2(segment):
+    right = top = -65536
+    left = bottom = 65536
+
+    for point in segment:
+        px, py = point
+        left = min(left, px)
+        right = max(right, px)
+        bottom = min(bottom, py)
+        top = max(top, py)
+
+    return [(left, top), (right, bottom)]
+
+def inBounds(bounds, point):
+    left, top = bounds[0]
+    right, bottom = bounds[1]
+    px, py = point
+
+    return left <= px <= right and bottom <= py <= top
 
 def isVertical(segment):
     dx, _ = getDeltas(segment)
@@ -35,6 +71,53 @@ def slope(segment):
 
     if dx == 0: return math.inf
     return dy / dx
+
+def midpoint(line):
+    p0x, p0y, p1x, p1y = endPoints(line)
+
+    return ((p0x + p1x) / 2, (p0y + p1y) / 2)
+
+def cooefs(line):
+    p0x, p0y, p1x, p1y = endPoints(line)
+
+    a = p0y - p1y
+    b = p1x - p0x
+    c = (p0x * p1y) - (p1x * p0y)
+
+    return (a, b, -c)
+
+def intersectionPoint(l1, l2):
+    a1, b1, c1 = cooefs(l1)
+    a2, b2, c2 = cooefs(l2)
+
+    d = a1 * b2 - b1 * a2
+
+    # d == 0 means lines are parallel
+    if d == 0: return None
+
+    dx = c1 * b2 - b1 * c2
+    dy = a1 * c2 - c1 * a2
+
+    intersection = (dx / d, dy / d)
+
+    b1 = bounds(l1)
+    b2 = bounds(l2)
+
+    return intersection if inBounds(b1, intersection) and inBounds(b2, intersection) else None
+
+def intersection(rectangle1, rectangle2):
+    l1, t1 = rectangle1[0]
+    r1, b1 = rectangle1[1]
+    l2, t2 = rectangle2[0]
+    r2, b2 = rectangle2[1]
+
+    li = max(l1, l2)
+    ti = min(t1, t2)
+    ri = min(r1, r2)
+    bi = max(b1, b2)
+
+    if ri < li or bi < ti: return None  # maybe want <=, >=?
+    return [(li, ti), (ri, bi)]
 
 def verticalLines(contour):
     return list(filter(lambda s: isVertical(s), contour))
@@ -80,9 +163,25 @@ l0 = [p0, p1]
 l1 = [p1, p2]
 l2 = [p0, p2]
 
+l3 = [(0, 0), (100, 100)]
+l4 = [(50, 200), (200, 50)]
+
 def test():
     print(f"length(l0) = {length(l0)}, length(l1) = {length(l1)}, length(l2) = {length(l2)}")
     print(f"slope(l0) = {slope(l0)}, slope(l1) = {slope(l1)}, slope(l2) = {slope(l2)}")
+    print(f"midpoint(l0) = {midpoint(l0)}, midpoint(l1) = {midpoint(l1)}, midpoint(l2) = {midpoint(l2)}")
+    print(f"intersection = {intersectionPoint([(0,1), (2,3)], [(2,3), (0,4)])}")
+    print(f"intersectionPoint(l0, l1) = {intersectionPoint(l0, l1)}")
+    print(f"intersectionPoint(l0, l2) = {intersectionPoint(l0, l2)}")
+    print(f"intersectionPoint(l1, l2) = {intersectionPoint(l1, l2)}")
+    print(f"intersectionPoint(l3, l4) = {intersectionPoint(l3, l4)}")
+
+    print(bounds(l0) == bounds2(l0))
+    print(bounds(l1) == bounds2(l1))
+    print(bounds(l2) == bounds2(l2))
+    print(bounds(l3) == bounds2(l3))
+    print(bounds(l4) == bounds2(l4))
+
     print(pointOnLine((150, 200), l2))
     print(pointOnLine((-300, -400), l2))
 
