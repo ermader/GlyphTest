6130b8492735230780e0f22a0c498a5b27e1602b
Eric Mader <emader@gmx.us>
2020-07-10 11:08:06 -1000

    Renamed Rectangle to BoundsRectangle, added static method to construct an "empty" rectangle, added area property, crossesX(), crossesY(), intersection(). Added VerticalLinesCrossing(), HorizontalLinesCrossing(). Changed stand-alone functions to make use of BoundingRectangle.

--- PathUtilities.py
+++ PathUtilities.py
@@ -8,7 +8,7 @@ Created on July 7, 2020
 
 import math
 
-class Rectangle:
+class BoundsRectangle:
     def __init__(self, *points):
         right = top = -32768
         left = bottom = 32768
@@ -23,11 +23,43 @@ class Rectangle:
         self.leftTop = (left, top)
         self.rightBottom = (right, bottom)
 
+    @staticmethod
+    def empty():
+        r = BoundsRectangle((0, 0), (0, 0))
+        r.leftTop = (32768, -32768)
+        r.rightBottom = (-32768, 32768)
+        return r
+
+    @staticmethod
+    def fromContour(contour):
+        bounds = BoundsRectangle.empty()
+        for segment in contour:
+            bounds = bounds.union(BoundsRectangle(*segment))
+
+        return bounds
+
+    @staticmethod
+    def fromCoutours(contours):
+        bounds = BoundsRectangle.empty()
+        for contour in contours:
+            bounds = bounds.union(BoundsRectangle.fromContour(contour))
+
+        return bounds
+
+    def __str__(self):
+        return f"[{self.leftTop}, {self.rightBottom}]"
+
+    @property
     def width(self):
         return self.rightBottom[0] - self.leftTop[0]
 
+    @property
     def height(self):
         return self.leftTop[1] - self.rightBottom[1]
+
+    @property
+    def area(self):
+        return self.width * self.height
 
     def enclosesPoint(self, point):
         left, top = self.leftTop
@@ -35,6 +67,18 @@ class Rectangle:
         px, py = point
 
         return left <= px <= right and bottom <= py <= top
+
+    def crossesX(self, x):
+        left, _ = self.leftTop
+        right, _ = self.rightBottom
+
+        return left <= x <= right
+
+    def crossesY(self, y):
+        _, top = self.leftTop
+        _, bottom = self.rightBottom
+
+        return bottom <= y <= top
 
     def union(self, other):
         left, top = self.leftTop
@@ -47,7 +91,22 @@ class Rectangle:
         newRight = max(right, oright)
         newBottom = min(bottom, obottom)
 
-        return Rectangle((newLeft, newTop), (newRight, newBottom))
+        return BoundsRectangle((newLeft, newTop), (newRight, newBottom))
+
+    def intersection(self, other):
+        left, top = self.leftTop
+        right, bottom = self.rightBottom
+        oleft, otop = other.leftTop
+        oright, obottom = other.rightBottom
+
+        newLeft = max(left, oleft)
+        newTop = min(top, otop)
+        newRight = min(right, oright)
+        newBottom = max(bottom, obottom)
+
+        if newRight < newLeft or newTop < newBottom: return None  # maybe want <=, >=?
+        return BoundsRectangle((newLeft, newTop), (newRight, newBottom))
+
 
 def minMax(a, b):
     return (a, b) if a <= b else (b, a)
@@ -62,48 +121,6 @@ def getDeltas(segment):
     p0x, p0y, p1x, p1y = endPoints(segment)
 
     return (p1x - p0x, p1y - p0y)
-
-# Fix this to work for curves too...
-def bounds(segment):
-    p0x, p0y, p1x, p1y = endPoints(segment)
-    left, right = minMax(p0x, p1x)
-    bottom, top = minMax(p0y, p1y)
-
-    return [(left, top), (right, bottom)]
-
-def bounds2(segment):
-    right = top = -65536
-    left = bottom = 65536
-
-    for point in segment:
-        px, py = point
-        left = min(left, px)
-        right = max(right, px)
-        bottom = min(bottom, py)
-        top = max(top, py)
-
-    return [(left, top), (right, bottom)]
-
-def contourBoundsRectangle(contour):
-    right = top = -32768
-    left = bottom = 32768
-
-    for segment in contour:
-        for point in segment:
-            px, py = point
-            left = min(left, px)
-            right = max(right, px)
-            bottom = min(bottom, py)
-            top = max(top, py)
-
-    return Rectangle((left, top), (right, bottom))
-
-def inBounds(bounds, point):
-    left, top = bounds[0]
-    right, bottom = bounds[1]
-    px, py = point
-
-    return left <= px <= right and bottom <= py <= top
 
 def isVertical(segment):
     dx, _ = getDeltas(segment)
@@ -155,24 +172,10 @@ def intersectionPoint(l1, l2):
 
     intersection = (dx / d, dy / d)
 
-    b1 = bounds(l1)
-    b2 = bounds(l2)
+    b1 = BoundsRectangle(*l1)
+    b2 = BoundsRectangle(*l2)
 
-    return intersection if inBounds(b1, intersection) and inBounds(b2, intersection) else None
-
-def intersection(rectangle1, rectangle2):
-    l1, t1 = rectangle1[0]
-    r1, b1 = rectangle1[1]
-    l2, t2 = rectangle2[0]
-    r2, b2 = rectangle2[1]
-
-    li = max(l1, l2)
-    ti = min(t1, t2)
-    ri = min(r1, r2)
-    bi = max(b1, b2)
-
-    if ri < li or bi < ti: return None  # maybe want <=, >=?
-    return [(li, ti), (ri, bi)]
+    return intersection if b1.enclosesPoint(intersection) and b2.enclosesPoint(intersection) else None
 
 def flatten(contours):
     segments = []
@@ -186,8 +189,14 @@ def flatten(contours):
 def verticalLines(contour):
     return list(filter(lambda s: isVertical(s), contour))
 
+def verticalLinesCrossing(contour, y):
+    return list(filter(lambda s: crossesY(s, y), sortByX((verticalLines(contour)))))
+
 def horizontalLines(contour):
     return list(filter(lambda s: isHorizontal(s), contour))
+
+def horizontalLinesCrossing(contour, x):
+    return list(filter(lambda s: crossesX(s, x), sortByY((horizontalLines(contour)))))
 
 def sortByX(contour):
     return sorted(contour, key=lambda s: s[0][0])
@@ -196,22 +205,16 @@ def sortByY(contour):
     return sorted(contour, key=lambda s: s[0][1])
 
 def crossesY(line, y):
-    y0 = line[0][1]
-    y1 = line[1][1]
-    return y0 < y < y1 if y0 < y1 else y1 < y < y0
+    return BoundsRectangle(*line).crossesY(y)
 
 def crossesX(line, x):
-    x0 = line[0][0]
-    x1 = line[1][0]
-    return x0 < x < x1 if x0 < x1 else x1 < x < x0
+    return BoundsRectangle(*line).crossesX(x)
 
 # There must be a better way to do this...
 def pointOnLine(point, line):
-    px, py = point
-    p0x, p0y = line[0]
-    p1x, p1y = line[1]
+    bounds = BoundsRectangle(*line)
 
-    return crossesX(line, px) and crossesY(line, py) and slope(line) == slope([line[0], point])
+    return bounds.enclosesPoint(point) and slope(line) == slope([line[0], point])
 
 # Helvetica Neue H
 hContours = [[[(78, 714), (78, 0)], [(78, 0), (173, 0)], [(173, 0), (173, 327)], [(173, 327), (549, 327)], [(549, 327), (549, 0)], [(549, 0), (644, 0)], [(644, 0), (644, 714)], [(644, 714), (549, 714)], [(549, 714), (549, 407)], [(549, 407), (173, 407)], [(173, 407), (173, 714)], [(173, 714), (78, 714)]]]
@@ -219,9 +222,8 @@ xContours = [[[(248, 367), (0, 0)], [(0, 0), (106, 0)], [(106, 0), (304, 295)], 
 
 newYorkHContours = [[[(414, 155), (414, 1289)], ((414, 1289), (414, 1331), (424.0, 1354.0)), ((424.0, 1354.0), (434, 1377), (463.5, 1389.0)), ((463.5, 1389.0), (493, 1401), (550, 1409)), [(550, 1409), (550, 1444)], [(550, 1444), (56, 1444)], [(56, 1444), (56, 1410)], ((56, 1410), (118, 1401), (148.0, 1388.5)), ((148.0, 1388.5), (178, 1376), (188.0, 1351.5)), ((188.0, 1351.5), (198, 1327), (198, 1283)), [(198, 1283), (198, 161)], ((198, 161), (198, 117), (188.0, 92.5)), ((188.0, 92.5), (178, 68), (148.0, 55.5)), ((148.0, 55.5), (118, 43), (56, 34)), [(56, 34), (56, 0)], [(56, 0), (550, 0)], [(550, 0), (550, 34)], ((550, 34), (493, 43), (463.5, 55.0)), ((463.5, 55.0), (434, 67), (424.0, 90.0)), ((424.0, 90.0), (414, 113), (414, 155))], [[(1352, 161), (1352, 1283)], ((1352, 1283), (1352, 1328), (1362.5, 1352.0)), ((1362.5, 1352.0), (1373, 1376), (1403.5, 1388.5)), ((1403.5, 1388.5), (1434, 1401), (1494, 1410)), [(1494, 1410), (1494, 1444)], [(1494, 1444), (1000, 1444)], [(1000, 1444), (1000, 1410)], ((1000, 1410), (1059, 1401), (1088.0, 1389.0)), ((1088.0, 1389.0), (1117, 1377), (1126.5, 1354.5)), ((1126.5, 1354.5), (1136, 1332), (1136, 1289)), [(1136, 1289), (1136, 155)], ((1136, 155), (1136, 113), (1126.5, 90.0)), ((1126.5, 90.0), (1117, 67), (1088.0, 55.0)), ((1088.0, 55.0), (1059, 43), (1000, 34)), [(1000, 34), (1000, 0)], [(1000, 0), (1494, 0)], [(1494, 0), (1494, 34)], ((1494, 34), (1434, 43), (1403.5, 55.5)), ((1403.5, 55.5), (1373, 68), (1362.5, 92.5)), ((1362.5, 92.5), (1352, 117), (1352, 161))], [[(306, 717), (1244, 717)], [(1244, 717), (1244, 762)], [(1244, 762), (306, 762)], [(306, 762), (306, 717)]]]
 
-height = 714
-h_25 = height * .25
-h_75 = height * .75
+hBounds = BoundsRectangle.fromCoutours(hContours)
+hHeight = hBounds.height
 
 p0 = (0, 0)
 p1 = (300, 0)
@@ -235,7 +237,7 @@ l4 = [(50, 200), (200, 50)]
 
 def test():
     print(f"length(l0) = {length(l0)}, length(l1) = {length(l1)}, length(l2) = {length(l2)}")
-    print(f"slope(l0) = {slope(l0)}, slope(l1) = {slope(l1)}, slope(l2) = {slope(l2)}")
+    print(f"slope(l0) = {slope(l0)}, slope(l1) = {slope(l1)}, slope(l2) = {slope(l2)}, slope([p2, p0]) = {slope([p2, p0])}")
     print(f"midpoint(l0) = {midpoint(l0)}, midpoint(l1) = {midpoint(l1)}, midpoint(l2) = {midpoint(l2)}")
     print(f"intersection = {intersectionPoint([(0,1), (4,5)], [(4, 2), (0,4)])}")
     print(f"intersectionPoint(l0, l1) = {intersectionPoint(l0, l1)}")
@@ -243,47 +245,37 @@ def test():
     print(f"intersectionPoint(l1, l2) = {intersectionPoint(l1, l2)}")
     print(f"intersectionPoint(l3, l4) = {intersectionPoint(l3, l4)}")
 
-    print(bounds(l0) == bounds2(l0))
-    print(bounds(l1) == bounds2(l1))
-    print(bounds(l2) == bounds2(l2))
-    print(bounds(l3) == bounds2(l3))
-    print(bounds(l4) == bounds2(l4))
-
     print(pointOnLine((150, 200), l2))
     print(pointOnLine((-300, -400), l2))
     print()
 
-    verticals = verticalLines(hContours[0])
-    sortedVerticals = sortByX(verticals)
-    vert_25 = list(filter(lambda s: crossesY(s, h_25), sortedVerticals))
-    glyphWidth = vert_25[-1][0][0] - vert_25[0][0][0]
-    print(f"vertical stroke width = {vert_25[1][0][0] - vert_25[0][0][0]}")
-    print(sortedVerticals)
+    hVerticals = verticalLinesCrossing(hContours[0], hBounds.height / 4)
+    hStroke = BoundsRectangle(*hVerticals[0], *hVerticals[1])
+    print(f"vertical stroke width of Helvetica Neue H = {hStroke.width}")
 
-    horizontals = horizontalLines(hContours[0])
-    sortedHorizontals = sortByY(horizontals)
-    horiz_50 = list(filter(lambda s: crossesX(s, glyphWidth/2), sortedHorizontals))
-    print(f"horizontal stroke width = {horiz_50[1][0][1] - horiz_50[0][0][1]}")
-    print(sortedHorizontals)
+    hHorizontals = horizontalLinesCrossing(hContours[0], hBounds.width / 2)
+    vStroke = BoundsRectangle(*hHorizontals[0], *hHorizontals[1])
+    print(f"horizontal stroke width of Helvetica Neue H = {vStroke.height}")
+    print()
 
     diagonals = list(filter(lambda s: not isHorizontal(s), xContours[0]))
-    diag_25 = list(filter(lambda s: crossesY(s, h_25), diagonals))
-    diag_75 = list(filter(lambda s: crossesY(s, h_75), diagonals))
+    diag_25 = list(filter(lambda s: crossesY(s, hBounds.height / 4), diagonals))
+    diag_75 = list(filter(lambda s: crossesY(s, hBounds.height * .75), diagonals))
 
-    print(diagonals)
+    print(f"diagonal strokes of Helvetica Neue X = {diagonals}")
+    print()
 
-    nyb = []
-    for contour in newYorkHContours:
-        for segment in contour:
-            nyb.append(Rectangle(*segment))
-
-    bb = nyb[0]
-    for b in nyb:
-        bb = bb.union(b)
-
+    nyhBounds =  BoundsRectangle.fromCoutours(newYorkHContours)
     newYorkHFlat = flatten(newYorkHContours)
-    nyhBounds =  contourBoundsRectangle(newYorkHFlat)
-    print(newYorkHFlat)
+
+    newYorkHVert = verticalLinesCrossing(newYorkHFlat, nyhBounds.height / 4)
+    vStroke = BoundsRectangle(*newYorkHVert[0], *newYorkHVert[1])
+    print(f"vertical stroke width of New York H = {vStroke.width}")
+
+    newYorkHHoriz = horizontalLinesCrossing(newYorkHFlat, nyhBounds.width / 2)
+    hStroke = BoundsRectangle(*newYorkHHoriz[0], *newYorkHHoriz[1])
+    print(f"horizontal stroke width of New York H = {hStroke.height}")
+    print(f"intersection of vertical and horizontal strokes = {vStroke.intersection(hStroke)}")
 
 if __name__ == "__main__":
     test()
