153ab226a703e40587fe271afd31a8e5ef81e012
Eric Mader <emader@gmx.us>
2021-02-09 12:17:14 -1000

    Add processing.

--- RasterSamplingTool.py
+++ RasterSamplingTool.py
@@ -9,30 +9,63 @@ Created on February 5, 2021
 import os
 import pathlib
 from sys import argv, exit, stderr
+from TestArgumentIterator import TestArgs
 import RasterSamplingTest
 
-def main():
+class RasterSamplingToolArgs(TestArgs):
+    def __init__(self):
+        self.inputDir = ""
+        self.outputDir = ""
+        TestArgs.__init__(self)
 
-    # inputDir = "/Users/emader/Downloads/TestFonts"
-    inputDir = "/Users/emader/TTCTest"
-    outDir = os.path.join(inputDir, "RasterSamplingTests")
+    @classmethod
+    def forArguments(cls, argumentList):
+        args = RasterSamplingToolArgs()
+        args.fontFile = "(dummy)"  # so TestArgumentIterator doesn't complain about missing "--font"...
+        args.processArguments(argumentList)
+        return args
+
+    def processArgument(self, argument, arguments):
+        if argument == "--input":
+            self.inputDir = arguments.nextExtra("input directory")
+        elif argument == "--output":
+            self.outputDir = arguments.nextExtra("output directory")
+        else:
+            TestArgs.processArgument(self, argument, arguments)
+
+
+def main():
+    argumentList = argv
+    args = None
+    programName = os.path.basename(argumentList.pop(0))
+    if len(argumentList) == 0:
+        print(__doc__, file=stderr)
+        exit(1)
+
+    try:
+        toolArgs = RasterSamplingToolArgs.forArguments(argumentList)
+    except ValueError as error:
+        print(programName + ": " + str(error), file=stderr)
+        exit(1)
 
     testCount = failedCount = 0
-    for path in pathlib.Path(inputDir).rglob("*.[ot]t[cf]"):
-        args = RasterSamplingTest.RasterSamplingTestArgs()
-        args.fontFile = str(path)
-        args.fontNumber = 0
-        args.glyphName = "l"
-        reldir = os.path.dirname(os.path.relpath(path, inputDir))
-        args.outdir = os.path.join(outDir, reldir)
-        args.silent = True
-        os.makedirs(args.outdir, exist_ok=True)
+    for path in pathlib.Path(toolArgs.inputDir).rglob("*.[ot]t[cf]"):
+        testArgs = RasterSamplingTest.RasterSamplingTestArgs()
+        testArgs.fontFile = str(path)
+        testArgs.fontNumber = 0
+        testArgs.glyphName = toolArgs.glyphName
+        testArgs.glyphID = toolArgs.glyphID
+        testArgs.charCode = toolArgs.charCode
+        reldir = os.path.dirname(os.path.relpath(path, toolArgs.inputDir))
+        testArgs.outdir = os.path.join(toolArgs.outputDir, reldir)
+        testArgs.silent = True
+        os.makedirs(testArgs.outdir, exist_ok=True)
 
-        print(f"{os.path.relpath(path, inputDir)}:")
+        print(f"{os.path.relpath(path, toolArgs.inputDir)}:")
 
         while True:
             try:
-                test = RasterSamplingTest.RasterSamplingTest(args)
+                test = RasterSamplingTest.RasterSamplingTest(testArgs)
                 test.run()
             except StopIteration:
                 break
@@ -41,8 +74,8 @@ def main():
                 print("Failed\n")
 
             testCount += 1
-            if not args.fontFile.endswith(".ttc"): break
-            args.fontNumber += 1
+            if not testArgs.fontFile.endswith(".ttc"): break
+            testArgs.fontNumber += 1
 
     print(f"{testCount} tests, {failedCount} failures.")
 
